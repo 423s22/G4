e3e262192fabc2047d75797f9671caa9810419f7
CodeAX2 <jacobhofer1@gmail.com>
2022-03-02 14:17:26 -0700

    Create dbMain.sql

---
+++ better_than_sum/SQL/dbMain.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Users (
+    userID BIGINT NOT NULL,
+    name VARCHAR(128),
+    PRIMARY KEY (userID)
+);
+
+CREATE TABLE Products (
+    productID BIGINT NOT NULL,
+    baseCost INTEGER NOT NULL,
+    name VARCHAR(64),
+    owningUser BIGINT NOT NULL,
+    PRIMARY KEY (productID),
+    CONSTRAINT FK_ProductUser FOREIGN KEY (owningUser) REFERENCES Users(userID)
+);
+
+CREATE TABLE VariationGroups (
+    groupID BIGINT NOT NULL,
+    name VARCHAR(64),
+    owningProduct BIGINT NOT NULL,
+    PRIMARY KEY (groupID),
+    CONSTRAINT FK_VariationProduct FOREIGN KEY (owningProduct) REFERENCES Products(productID)
+);
+
+CREATE TABLE Variations (
+    variationID BIGINT NOT NULL,
+    addedCost INTEGER NOT NULL,
+    name VARCHAR(64),
+    owningGroup BIGINT NOT NULL,
+    PRIMARY KEY (variationID),
+    CONSTRAINT FK_VariationGroup FOREIGN KEY (owningGroup) REFERENCES VariationGroups(groupID)
+);
+
+CREATE TABLE Combos (
+    comboID BIGINT NOT NULL,
+    stockAmount INTEGER NOT NULL,
+    PRIMARK KEY (comboID)
+);
+
+CREATE TABLE ComboItems (
+    owningCombo BIGINT NOT NULL,
+    variationItem BIGINT NOT NULL,
+    CONSTRAINT FK_ComboVariation FOREIGN KEY (variationItem) REFERENCES Variations(variationID),
+    CONSTRAINT FK_Combo FOREIGN KEY (owningCombo) REFERENCES Combos(comboID)
+);
+
+CREATE TABLE VariationBlockers (
+    excludeVariationA BIGINT NOT NULL,
+    excludeVariationB BIGINT NOT NULL,
+    CONSTRAINT FK_VarA FOREIGN KEY (excludeVariationA) REFERENCES Variations(variationID),
+    CONSTRAINT FK_VarB FOREIGN KEY (excludeVariationB) REFERENCES Variations(variationID),
+);
