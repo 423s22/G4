dc985c18ff098fd5f58825fa89b5a607d27d85e1
CodeAX2 <jacobhofer1@gmail.com>
2022-03-07 14:39:11 -0700

    Modify browser/server testing

--- better_than_sum/pages/index.js
+++ better_than_sum/pages/index.js
@@ -2,16 +2,15 @@ import { Heading, Page } from "@shopify/polaris";
 
 export default function Index() {
 
-	// Any code here runs on the server, so the followling line would throw an error
-	//console.log(window.location.href);
+	if (typeof window === "object") {
+		console.log("Runs in browser: " + window.location.href);
+	} else {
+		console.log("Runs on server");
+	}
 
 	return (
 
 		<Page>
-			<script>
-				// This runs in the browser
-				console.log("Hello!");
-			</script>
 			<Heading>
 				Shopify app with Node and React{" "}
 				<span role="img" aria-label="tada emoji">
