59dc197bed3d2f86110ed2cd332c290644dfd53c
CodeAX2 <jacobhofer1@gmail.com>
2022-03-08 16:21:21 -0700

    Add basic functionality outline for a database get request

--- better_than_sum/pages/database/get/index.js
+++ better_than_sum/pages/database/get/index.js
@@ -1,26 +1,8 @@
-import { useSearchParams } from "react-router-native";
-
 export default function Index() {
 
     if (typeof window !== "object") {
 
-        let [searchParams, setSearchParams] = useSearchParams();
-        console.log(searchParams);
-
-        const mysql = require("mysql");
-
-        let conn = mysql.createConnection({
-            host: "localhost",
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASS
-        });
-
-        conn.connect(function(err) {
-            if (err)
-                console.log(err);
-            else
-                console.log("Connected!");
-        });
+        console.log("in db get file")
     }
 
     return (
