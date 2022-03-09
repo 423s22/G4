fb5529097e478d392b62ddf2019ecbeb1cb56e7d
CodeAX2 <jacobhofer1@gmail.com>
2022-03-09 11:18:55 -0700

    Change location of app creation

--- better_than_sum/pages/index.js
+++ better_than_sum/pages/index.js
@@ -3,11 +3,8 @@ import {App} from "./../server/AdminApp/App.js";
 
 export default function Index() {
 
-	if (typeof window === "object") {
-		console.log(App);
-		let app = new App();
-		app.start();
-	}
+	let app = new App();
+	app.start();
 
 	return (
 		<Page>
