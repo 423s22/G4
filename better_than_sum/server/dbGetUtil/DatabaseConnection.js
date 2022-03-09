9e4efbd69861f8ea9fbf19de0ac229f72b98e6a1
Jacob Hofer <jacobhofer1@gmail.com>
2022-03-08 17:52:31 -0700

    Properly use mysql-await

--- better_than_sum/server/dbGetUtil/DatabaseConnection.js
+++ better_than_sum/server/dbGetUtil/DatabaseConnection.js
@@ -1,37 +1,33 @@
 const mysql = require("mysql-await");
 
 export default class DatabaseConnection {
-    constructor(host, username, password, database) {
-        this._connection = mysql.createConnection({
-            host: host,
-            user: username,
-            password: password,
-            database: database,
-        });
-    }
+  constructor(host, username, password, database) {
+    this._connection = mysql.createConnection({
+      host: host,
+      user: username,
+      password: password,
+      database: database,
+    });
+  }
 
-    async getUserProductsJSON(userID) {
-        let results = await this._connection.awaitQuery(
-            "SELECT Products.productID FROM Products INNER JOIN Users ON Users.userID = Products.owningUser WHERE Users.userID = ?;", [userID]
-        );
+  async getUserProductsJSON(userID) {
+    let results = await this._connection.awaitQuery(
+      "SELECT Products.productID FROM Products INNER JOIN Users ON Users.userID = Products.owningUser WHERE Users.userID = ?;",
+      [userID]
+    );
 
-        console.log(results);
-    }
+    console.log(results);
+  }
 
-    async connect() {
-        this._connection.connect(function(err) {
-            if (err) {
-                this._lastError = err;
-                this._isConnected = false;
-            } else {
-                this._lastError = null;
-                this._isConnected = true;
-                console.log("Connected!");
-                let results = this._connection.awaitQuery(
-                    "SELECT Products.productID FROM Products INNER JOIN Users ON Users.userID = Products.owningUser WHERE Users.userID = ?;", [userID]
-                );
-                console.log(results);
-            }
-        });
-    }
+  async connect() {
+    await this._connection.awaitConnect(async function (err) {
+      if (err) {
+        this._lastError = err;
+        this._isConnected = false;
+      } else {
+        this._lastError = null;
+        this._isConnected = true;
+      }
+    });
+  }
 }
