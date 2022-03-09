4971e020fcff2cb12061c650969adca3dae747bd
CodeAX2 <jacobhofer1@gmail.com>
2022-03-08 17:53:56 -0700

    Add call to getProducts

--- better_than_sum/server/server.js
+++ better_than_sum/server/server.js
@@ -12,19 +12,19 @@ dotenv.config();
 const port = parseInt(process.env.PORT, 10) || 8081;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({
-  dev,
+    dev,
 });
 const handle = app.getRequestHandler();
 
 Shopify.Context.initialize({
-  API_KEY: process.env.SHOPIFY_API_KEY,
-  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-  SCOPES: process.env.SCOPES.split(","),
-  HOST_NAME: process.env.HOST.replace(/https:\/\/|\/$/g, ""),
-  API_VERSION: ApiVersion.October20,
-  IS_EMBEDDED_APP: true,
-  // This should be replaced with your preferred storage strategy
-  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+    API_KEY: process.env.SHOPIFY_API_KEY,
+    API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+    SCOPES: process.env.SCOPES.split(","),
+    HOST_NAME: process.env.HOST.replace(/https:\/\/|\/$/g, ""),
+    API_VERSION: ApiVersion.October20,
+    IS_EMBEDDED_APP: true,
+    // This should be replaced with your preferred storage strategy
+    SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
 });
 
 // Storing the currently active shops in memory will force them to re-login when your server restarts. You should
@@ -32,100 +32,102 @@ Shopify.Context.initialize({
 const ACTIVE_SHOPIFY_SHOPS = {};
 
 Shopify.Webhooks.Registry.addHandler("APP_UNINSTALLED", {
-  path: "/webhooks",
-  webhookHandler: async (topic, shop, body) =>
-    delete ACTIVE_SHOPIFY_SHOPS[shop],
+    path: "/webhooks",
+    webhookHandler: async(topic, shop, body) =>
+        delete ACTIVE_SHOPIFY_SHOPS[shop],
 });
 
-app.prepare().then(async () => {
-  const server = new Koa();
-  const router = new Router();
-  server.keys = [Shopify.Context.API_SECRET_KEY];
-  server.use(
-    createShopifyAuth({
-      async afterAuth(ctx) {
-        // Access token and shop available in ctx.state.shopify
-        const { shop, accessToken, scope } = ctx.state.shopify;
-        const host = ctx.query.host;
-        ACTIVE_SHOPIFY_SHOPS[shop] = scope;
+app.prepare().then(async() => {
+    const server = new Koa();
+    const router = new Router();
+    server.keys = [Shopify.Context.API_SECRET_KEY];
+    server.use(
+        createShopifyAuth({
+            async afterAuth(ctx) {
+                // Access token and shop available in ctx.state.shopify
+                const { shop, accessToken, scope } = ctx.state.shopify;
+                const host = ctx.query.host;
+                ACTIVE_SHOPIFY_SHOPS[shop] = scope;
 
-        const responses = await Shopify.Webhooks.Registry.register({
-          shop,
-          accessToken,
-          path: "/webhooks",
-          topic: "APP_UNINSTALLED",
-        });
+                const responses = await Shopify.Webhooks.Registry.register({
+                    shop,
+                    accessToken,
+                    path: "/webhooks",
+                    topic: "APP_UNINSTALLED",
+                });
 
-        if (!responses["APP_UNINSTALLED"].success) {
-          console.log(
-            `Failed to register APP_UNINSTALLED webhook: ${responses.result}`
-          );
+                if (!responses["APP_UNINSTALLED"].success) {
+                    console.log(
+                        `Failed to register APP_UNINSTALLED webhook: ${responses.result}`
+                    );
+                }
+
+                // Redirect to app with shop parameter upon auth
+                ctx.redirect(`/?shop=${shop}&host=${host}`);
+            },
+        })
+    );
+
+    const handleRequest = async(ctx) => {
+        await handle(ctx.req, ctx.res);
+        ctx.respond = false;
+        ctx.res.statusCode = 200;
+    };
+
+    router.post("/webhooks", async(ctx) => {
+        try {
+            await Shopify.Webhooks.Registry.process(ctx.req, ctx.res);
+            console.log(`Webhook processed, returned status code 200`);
+        } catch (error) {
+            console.log(`Failed to process webhook: ${error}`);
         }
+    });
 
-        // Redirect to app with shop parameter upon auth
-        ctx.redirect(`/?shop=${shop}&host=${host}`);
-      },
-    })
-  );
+    router.post(
+        "/graphql",
+        verifyRequest({ returnHeader: true }),
+        async(ctx, next) => {
+            await Shopify.Utils.graphqlProxy(ctx.req, ctx.res);
+        }
+    );
 
-  const handleRequest = async (ctx) => {
-    await handle(ctx.req, ctx.res);
-    ctx.respond = false;
-    ctx.res.statusCode = 200;
-  };
+    router.get("(/_next/static/.*)", handleRequest); // Static content is clear
+    router.get("/_next/webpack-hmr", handleRequest); // Webpack content is clear
 
-  router.post("/webhooks", async (ctx) => {
-    try {
-      await Shopify.Webhooks.Registry.process(ctx.req, ctx.res);
-      console.log(`Webhook processed, returned status code 200`);
-    } catch (error) {
-      console.log(`Failed to process webhook: ${error}`);
-    }
-  });
+    let dbConn = new DatabaseConnection(
+        "localhost",
+        process.env.MYSQL_USER,
+        process.env.MYSQL_PASS,
+        "G4db"
+    );
 
-  router.post(
-    "/graphql",
-    verifyRequest({ returnHeader: true }),
-    async (ctx, next) => {
-      await Shopify.Utils.graphqlProxy(ctx.req, ctx.res);
-    }
-  );
+    dbConn.connect().then(() => {
+        dbConn.getUserProductsJSON();
+    });
 
-  router.get("(/_next/static/.*)", handleRequest); // Static content is clear
-  router.get("/_next/webpack-hmr", handleRequest); // Webpack content is clear
+    router.get("/database/get/", async(ctx) => {
+        // Handle get request from database
 
-  let dbConn = new DatabaseConnection(
-    "localhost",
-    process.env.MYSQL_USER,
-    process.env.MYSQL_PASS,
-    "G4db"
-  );
-  dbConn.connect();
+        // Check the shop is active
+        if (ACTIVE_SHOPIFY_SHOPS[ctx.query.shop] === undefined) {
+            ctx.redirect(`/auth?shop=${shop}`);
+        } else {}
+    });
 
-  router.get("/database/get/", async (ctx) => {
-    // Handle get request from database
+    router.get("(.*)", async(ctx) => {
+        const shop = ctx.query.shop;
 
-    // Check the shop is active
-    if (ACTIVE_SHOPIFY_SHOPS[ctx.query.shop] === undefined) {
-      ctx.redirect(`/auth?shop=${shop}`);
-    } else {
-    }
-  });
+        // This shop hasn't been seen yet, go through OAuth to create a session
+        if (ACTIVE_SHOPIFY_SHOPS[shop] === undefined) {
+            ctx.redirect(`/auth?shop=${shop}`);
+        } else {
+            await handleRequest(ctx);
+        }
+    });
 
-  router.get("(.*)", async (ctx) => {
-    const shop = ctx.query.shop;
-
-    // This shop hasn't been seen yet, go through OAuth to create a session
-    if (ACTIVE_SHOPIFY_SHOPS[shop] === undefined) {
-      ctx.redirect(`/auth?shop=${shop}`);
-    } else {
-      await handleRequest(ctx);
-    }
-  });
-
-  server.use(router.allowedMethods());
-  server.use(router.routes());
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+    server.use(router.allowedMethods());
+    server.use(router.routes());
+    server.listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`);
+    });
 });
