2f66f8508d3f9cfc35d034055c37633a62e583dd
Jacob Hofer <jacobhofer1@gmail.com>
2022-03-08 17:38:16 -0700

    Add mysql-await

--- better_than_sum/server/dbGetUtil/DatabaseConnection.js
+++ better_than_sum/server/dbGetUtil/DatabaseConnection.js
@@ -1,36 +1,39 @@
 const mysql = require("mysql");
 
 export default class DatabaseConnection {
-    constructor(host, username, password, database) {
-        this._connection = mysql.createConnection({
-            host: host,
-            user: username,
-            password: password,
-            database: database
-        });
-    }
+  constructor(host, username, password, database) {
+    this._connection = mysql.createConnection({
+      host: host,
+      user: username,
+      password: password,
+      database: database,
+    });
+  }
 
-    async getUserProductsJSON(userID) {
-        let results = await this._connection.query(
-            "SELECT Products.productID FROM Products INNER JOIN Users ON Users.userID = Products.owningUser WHERE Users.userID = ?;", [userID]);
+  async getUserProductsJSON(userID) {
+    let results = await this._connection.query(
+      "SELECT Products.productID FROM Products INNER JOIN Users ON Users.userID = Products.owningUser WHERE Users.userID = ?;",
+      [userID]
+    );
 
+    console.log(results);
+  }
+
+  async connect() {
+    this._connection.connect(function (err) {
+      if (err) {
+        this._lastError = err;
+        this._isConnected = false;
+      } else {
+        this._lastError = null;
+        this._isConnected = true;
+        console.log("Connected!");
+        let results = this._connection.query(
+          "SELECT Products.productID FROM Products INNER JOIN Users ON Users.userID = Products.owningUser WHERE Users.userID = ?;",
+          [userID]
+        );
         console.log(results);
-    }
-
-    connect() {
-        this._connection.connect(function(err) {
-            if (err) {
-                this._lastError = err;
-                this._isConnected = false;
-            } else {
-                this._lastError = null;
-                this._isConnected = true;
-                console.log("Connected!");
-                let results = await this._connection.query(
-                    "SELECT Products.productID FROM Products INNER JOIN Users ON Users.userID = Products.owningUser WHERE Users.userID = ?;", [userID]);
-                console.log(results);
-            }
-        });
-    }
-
+      }
+    });
+  }
 }
