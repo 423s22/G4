6a6726e75c9ea8c7b61b2a21b273ffd77c94cbe2
CodeAX2 <jacobhofer1@gmail.com>
2022-03-10 12:25:13 -0700

    Change another SQL typo

--- better_than_sum/server/dbGetUtil/DatabaseConnection.js
+++ better_than_sum/server/dbGetUtil/DatabaseConnection.js
@@ -67,7 +67,7 @@ export default class DatabaseConnection {
 
     async _getVariationGroupsJSON(productID) {
         let results = await this._connection.awaitQuery(
-            `SELECT * FROM Variations WHERE owningProduct = ?`, [productID]
+            `SELECT * FROM Variations WHERE owningGroup = ?`, [productID]
         );
         return JSON.stringify(results);
     }
