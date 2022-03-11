735cdccb9a3725c28f66c655ab2b086a462fbde6
Jacob Hofer <jacobhofer1@gmail.com>
2022-03-11 13:47:11 -0700

    Change test to use require

--- better_than_sum/server/dbAPI/DatabaseConnection.test.js
+++ better_than_sum/server/dbAPI/DatabaseConnection.test.js
@@ -1,8 +1,7 @@
-import DatabaseConnection from "./DatabaseConnection";
+const DatabaseConnection = require("./DatabaseConnection");
 
 test("Tests database connection", () => {
     let dbConn = new DatabaseConnection("localhost", "testUser", "testPassword", "G4db");
     let id = JSON.parse(dbConn._getUserIDJSON("Test User"))["userID"];
     expect(id).toBe(1);
-    // Add comment for another push
 });
