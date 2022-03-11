36f8879ebc86a9843238fe5e0c6e7398bd0c41d3
Jacob Hofer <jacobhofer1@gmail.com>
2022-03-11 13:43:17 -0700

    New push for testing

--- better_than_sum/server/dbAPI/DatabaseConnection.test.js
+++ better_than_sum/server/dbAPI/DatabaseConnection.test.js
@@ -4,4 +4,5 @@ test("Tests database connection", () => {
     let dbConn = new DatabaseConnection("localhost", "testUser", "testPassword", "G4db");
     let id = JSON.parse(dbConn._getUserIDJSON("Test User"))["userID"];
     expect(id).toBe(1);
+    // Add comment for another push
 });
