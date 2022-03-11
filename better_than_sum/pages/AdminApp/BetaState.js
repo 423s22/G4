0542270a09b18ec4a517ee2c6045242e1562e839
CodeAX2 <jacobhofer1@gmail.com>
2022-03-11 14:08:30 -0700

    Fix formatting and output window href

--- better_than_sum/pages/AdminApp/BetaState.js
+++ better_than_sum/pages/AdminApp/BetaState.js
@@ -1,22 +1,26 @@
 import AppState from "./AppState";
 
 export default class BetaState extends AppState {
-  constructor(app) {
-    super(app);
-  }
+    constructor(app) {
+        super(app);
+    }
 
-  onEnable() {
-    console.log("Enabled!");
-  }
+    onEnable() {
+        console.log("Enabled!");
+    }
 
-  onDisable() {
-    console.log("Disabled!");
-  }
+    onDisable() {
+        console.log("Disabled!");
+    }
 
-  onRender(divID) {
-    let appDiv = document.getElementById(divID);
-    appDiv.innerHTML = `
-        <h1>Welcome to Better than Sum</h1>
+    onRender(divID) {
+        let appDiv = document.getElementById(divID);
+
+        //let req = new XMLHttpRequest();
+        //req.open("GET")
+
+        appDiv.innerHTML = `
+        <h1>${window.location.href}</h1>
         `;
-  }
+    }
 }
