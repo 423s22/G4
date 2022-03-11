36971839c5def6f18e46389b6587dadafd4cbadd
Jacob Hofer <jacobhofer1@gmail.com>
2022-03-11 13:41:36 -0700

    Add basic test setup

---
+++ better_than_sum/server/dbAPI/DatabaseConnection.test.js
@@ -0,0 +1,7 @@
+import DatabaseConnection from "./DatabaseConnection";
+
+test("Tests database connection", () => {
+    let dbConn = new DatabaseConnection("localhost", "testUser", "testPassword", "G4db");
+    let id = JSON.parse(dbConn._getUserIDJSON("Test User"))["userID"];
+    expect(id).toBe(1);
+});
