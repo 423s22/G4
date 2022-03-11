566af5bcc2630dfe6031e3bb87cc3e6bdf9ccdfb
Faith Nelson <ufaithe@gmail.com>
2022-03-11 11:24:16 -0700

    Merge branch 'clientUI' of https://github.com/423s22/G4 into clientUI

--- better_than_sum/pages/AdminApp/BetaState.js
+++ better_than_sum/pages/AdminApp/BetaState.js
@@ -1,24 +1,22 @@
 import AppState from "./AppState";
 
 export default class BetaState extends AppState {
+  constructor(app) {
+    super(app);
+  }
 
-    constructor(app) {
-        super(app);
-    }
+  onEnable() {
+    console.log("Enabled!");
+  }
 
-    onEnable() {
-        console.log("Enabled!");
-    }
+  onDisable() {
+    console.log("Disabled!");
+  }
 
-    onDisable() {
-        console.log("Disabled!");
-    }
-
-    onRender(divID) {
-        let appDiv = document.getElementById(divID);
-        appDiv.innerHTML = `
-        <h1>Hello World!</h1>
+  onRender(divID) {
+    let appDiv = document.getElementById(divID);
+    appDiv.innerHTML = `
+        <h1>Welcome to Better than Sum</h1>
         `;
-    }
-
+  }
 }
