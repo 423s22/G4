88a9eb5038ccc24e7e1a02c9e15f09ec51ea97d8
Jacob Hofer <jacobhofer1@gmail.com>
2022-03-25 09:34:05 -0600

    Update Jest test to be async

--- better_than_sum/server/dbAPI/DatabaseConnection.test.js
+++ better_than_sum/server/dbAPI/DatabaseConnection.test.js
@@ -1,9 +1,10 @@
 import DatabaseConnection from "./DatabaseConnection";
 
-test("Tests database connection", () => {
+test("Tests database connection", (done) => {
     let dbConn = new DatabaseConnection("127.0.0.1", "root", "rootPassword", "G4db");
-    let results = dbConn._getUserIDJSON("Test User");
-    console.log(results);
-    let id = JSON.parse(results)["userID"];
-    expect(id).toBe(1);
+    dbConn._getUserIDJSON("Test User").then((results) => {
+        let id = JSON.parse(results)["userID"];
+        expect(id).toBe(1);
+        done();
+    });
 });
