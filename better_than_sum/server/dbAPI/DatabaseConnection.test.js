7c23fbfbe08b057e36d530d327bffa7c95ecf2f9
Jacob Hofer <jacobhofer1@gmail.com>
2022-03-25 09:36:56 -0600

    Fix Jest test to work for promises

--- better_than_sum/server/dbAPI/DatabaseConnection.test.js
+++ better_than_sum/server/dbAPI/DatabaseConnection.test.js
@@ -1,10 +1,10 @@
 import DatabaseConnection from "./DatabaseConnection";
 
-test("Tests database connection", (done) => {
+test("Tests database connection", () => {
     let dbConn = new DatabaseConnection("127.0.0.1", "root", "rootPassword", "G4db");
-    dbConn._getUserIDJSON("Test User").then((results) => {
+    return dbConn._getUserIDJSON("Test User").then((results) => {
         let id = JSON.parse(results)["userID"];
+        console.log(results);
         expect(id).toBe(1);
-        done();
     });
 });
