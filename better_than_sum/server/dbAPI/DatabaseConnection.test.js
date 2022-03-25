92f7f7b4f54dc2949547916200e2c589c57dd31e
Jacob Hofer <jacobhofer1@gmail.com>
2022-03-25 09:28:37 -0600

    Add debug output for test

--- better_than_sum/server/dbAPI/DatabaseConnection.test.js
+++ better_than_sum/server/dbAPI/DatabaseConnection.test.js
@@ -2,6 +2,8 @@ import DatabaseConnection from "./DatabaseConnection";
 
 test("Tests database connection", () => {
     let dbConn = new DatabaseConnection("127.0.0.1", "root", "rootPassword", "G4db");
-    let id = JSON.parse(dbConn._getUserIDJSON("Test User"))["userID"];
+    let results = dbConn._getUserIDJSON("Test User");
+    console.log(results);
+    let id = JSON.parse(results)["userID"];
     expect(id).toBe(1);
 });
