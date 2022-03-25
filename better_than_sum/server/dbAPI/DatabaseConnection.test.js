b8c93f707bab4382e65ec30cbd552c2646ce8202
Jacob Hofer <jacobhofer1@gmail.com>
2022-03-25 09:25:24 -0600

    Update test to login to MySQL as root

--- better_than_sum/server/dbAPI/DatabaseConnection.test.js
+++ better_than_sum/server/dbAPI/DatabaseConnection.test.js
@@ -1,7 +1,7 @@
 import DatabaseConnection from "./DatabaseConnection";
 
 test("Tests database connection", () => {
-    let dbConn = new DatabaseConnection("localhost", "testUser", "testPassword", "G4db");
+    let dbConn = new DatabaseConnection("127.0.0.1", "root", "rootPassword", "G4db");
     let id = JSON.parse(dbConn._getUserIDJSON("Test User"))["userID"];
     expect(id).toBe(1);
 });
