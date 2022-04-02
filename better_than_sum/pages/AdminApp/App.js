880a78b6bebf6a35f4d6a933d073fa28b58f2eb3
Faith Nelson <ufaithe@gmail.com>
2022-04-02 10:34:28 -0600

    Created bare bones of the site

--- better_than_sum/pages/AdminApp/App.js
+++ better_than_sum/pages/AdminApp/App.js
@@ -1,20 +1,54 @@
-import BetaState from "./BetaState";
+import AppState from "./AppState";
+import Product from "./Product";
 
 export default class App {
-    constructor() {
-        console.log("constructed");
-        this._state = new BetaState(this);
-        this._state.onEnable();
-        this._running = false;
-    }
+  constructor() {
+    console.log("constructed");
+    this._state = new AppState(this); // This will run the AppState class that will contain
+    //this._allStates = Map<AppStateType, _state>;
+    this._state.onEnable();
+    this._running = false;
+  }
 
-    start() {
-        this._running = true;
-        this._state.onRender("appDiv");
-    }
+  // TODO: Create setState() - Change state of app
 
-    isRunning() {
-        return this._running;
-    }
+  start() {
+    this._running = true;
+    // Created 2 divs, 1) navDiv = navBar &
+    // 2) stateDiv - going to populate information
+    // make sure to clear all childs of this in order to re-onRender()
+    let appDiv = document.getElementById("appDiv");
+    let navDiv = document.createElement("div");
+    appDiv.appendChild(navDiv);
+    navBar.id = "navDiv";
+
+    let stateDiv = document.createElement("div");
+    appDiv.appendChild(stateDiv);
+    stateDiv.id = "stateDiv";
+
+    // Adding the nav bar to
+
+    navItem = document.createElement("li");
+    navLink = document.createElement("a");
+
+    // Set properties on anchor
+    navLink.href = "DashboardState.js";
+    navLink.innerHTML = "Dashboard";
+
+    // add to div
+
+    navDiv.appendChild(navItem);
+    navDiv.appendChild(navLink);
+
+    this._state.onRender("appDiv");
+  }
+
+  isRunning() {
+    return this._running;
+  }
+
+  // TODO: Add in call to database
+
+  //TODO: Add in the call to products as a list
 }
 // TODO: Implement
---
+++ better_than_sum/pages/AdminApp/Product.js
@@ -0,0 +1,14 @@
+export default class Product {
+  constructor(id, db) {
+    // TODO: Create product to take in id & db
+    this._productID = id;
+    this.baseCost = null;
+    this.name = null;
+  }
+
+  //TODO: Create a save function
+
+  //TODO: refresh function
+
+  //TODO: getById function
+}
