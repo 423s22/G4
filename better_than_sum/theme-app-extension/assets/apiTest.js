d87ba94797b71797c4d267977c16dd620d259add
CodeAX2 <jacobhofer1@gmail.com>
2022-04-05 10:41:31 -0600

    Update database request

--- better_than_sum/theme-app-extension/assets/apiTest.js
+++ better_than_sum/theme-app-extension/assets/apiTest.js
@@ -2,9 +2,20 @@ function start() {
 
     dbURL = document.getElementById("dbURL").value;
 
-    fetch(dbURL + "/database?request=userProducts&userID=1").then((response) => {
-        console.log(response);
-    });
+
+    let url = new URL(dbURL);
+    url.pathname += "/database/";
+    url.searchParams.append("request", "userProducts");
+    url.searchParams.append("userID", "1");
+
+    let req = new XMLHttpRequest();
+    req.open("GET", url.toString());
+    req.onload = () => {
+        let responseJSON = JSON.parse(req.responseText);
+        console.log(responseJSON);
+    }
+    req.send();
+
 }
 
 start();
