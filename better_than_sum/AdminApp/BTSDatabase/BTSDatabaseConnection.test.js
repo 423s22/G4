18bcb9f257d56de2ffcf26f780e962edae537fbf
CodeAX2 <jacobhofer1@gmail.com>
2022-04-06 14:44:03 -0600

    Fix test to disconnect from database

--- better_than_sum/AdminApp/BTSDatabase/BTSDatabaseConnection.test.js
+++ better_than_sum/AdminApp/BTSDatabase/BTSDatabaseConnection.test.js
@@ -4,7 +4,7 @@ import BTSDatabase from "./DatabaseConnection";
 test("Tests BTS App Database Connection", () => {
 
     let serverDB = new ServerDatabase("127.0.0.1", "root", "rootPassword", "G4db");
-    return serverDB.connect().then(() => {
+    return serverDB.connect().then(async() => {
 
         let appDB = new BTSDatabase("127.0.0.1");
         let product = await appDB.createNewProduct(1);
@@ -17,6 +17,8 @@ test("Tests BTS App Database Connection", () => {
 
         let products = await appDB.getUserProducts(1);
         expect(products.length).toBe(2);
+
+        return serverDB.disconnect();
 
     });
 
