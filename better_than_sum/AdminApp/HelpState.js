32c4a4cf4810c7bf25868e3d0262142f7d81412b
Faith Nelson <ufaithe@gmail.com>
2022-04-09 11:58:18 -0600

    Finished the structure for Help Page

--- better_than_sum/AdminApp/HelpState.js
+++ better_than_sum/AdminApp/HelpState.js
@@ -2,8 +2,6 @@ import AppState from "./AppState";
 export default class HelpState extends AppState {
   constructor(app) {
     super(app);
-    const { Remarkable } = require("remarkable");
-    this._md = new Remarkable();
   }
 
   onEnable() {}
@@ -13,12 +11,16 @@ export default class HelpState extends AppState {
   onRender(divID) {
     let div = document.getElementById(divID);
     div.innerHTML = "";
+    div.innerHTML = "<h1>Help!</h1>";
 
     let xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         // Typical action to be performed when the document is ready:
-        div.innerHTML = xmlHttp.responseText;
+        //div.innerHTML = xmlHttp.responseText;
+        const { Remarkable } = require("remarkable");
+        this._md = new Remarkable();
+        div.innerHTML = this._md.render(xmlHttp.responseText);
       }
     };
     xmlHttp.open(
@@ -27,7 +29,5 @@ export default class HelpState extends AppState {
       true
     );
     xmlHttp.send();
-
-    div.innerHTML = "<h1>Help!</h1>";
   }
 }
