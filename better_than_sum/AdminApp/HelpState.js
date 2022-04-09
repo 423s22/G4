53f348132ad0aba13cc951b4061cf3d16434fcbb
Faith Nelson <ufaithe@gmail.com>
2022-04-09 11:49:25 -0600

    Was able to get file to print to page still needs to be formatted

--- better_than_sum/AdminApp/HelpState.js
+++ better_than_sum/AdminApp/HelpState.js
@@ -1,9 +1,11 @@
 import AppState from "./AppState";
-import "./../../Documentation/User Docs.md";
 export default class HelpState extends AppState {
   constructor(app) {
     super(app);
+    const { Remarkable } = require("remarkable");
+    this._md = new Remarkable();
   }
+
   onEnable() {}
 
   onDisable() {}
@@ -11,6 +13,20 @@ export default class HelpState extends AppState {
   onRender(divID) {
     let div = document.getElementById(divID);
     div.innerHTML = "";
+
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        div.innerHTML = xmlHttp.responseText;
+      }
+    };
+    xmlHttp.open(
+      "GET",
+      "https://raw.githubusercontent.com/423s22/G4/main/Documentation/Dev%20Docs.md",
+      true
+    );
+    xmlHttp.send();
 
     div.innerHTML = "<h1>Help!</h1>";
   }
