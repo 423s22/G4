44d39085a087bc95ce5733a235bffb5fc6703150
Faith Nelson <ufaithe@gmail.com>
2022-04-10 11:23:33 -0600

    Finished the general functionailty on the dashboard CSS STILL TO COME

--- better_than_sum/AdminApp/DashboardState.js
+++ better_than_sum/AdminApp/DashboardState.js
@@ -13,6 +13,9 @@ export default class DashboardState extends AppState {
     let div = document.getElementById(divID);
     div.innerHTML = "";
 
-    div.innerHTML = "<h1>Welcome, $name!</h1>";
+    div.innerHTML = "";
+    let codeHtml =
+      "<h1>Welcome, $name!</h1> <h2>To see your add or edit your products list: </h2> <br> <li>See Product tab</li> <br> <h2>To find help: </h2> <br> <li>See Help tab</li> <br> <h2>To see your see or edit your settings information</h2> <br> <li>See Settings tab</li>";
+    div.innerHTML = codeHtml;
   }
 }
