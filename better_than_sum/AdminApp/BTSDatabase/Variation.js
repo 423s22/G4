ade2b5e2186c91c71d4b4cf6856bb10360be829f
CodeAX2 <jacobhofer1@gmail.com>
2022-04-11 14:10:52 -0600

    Update documentation

--- better_than_sum/AdminApp/BTSDatabase/Variation.js
+++ better_than_sum/AdminApp/BTSDatabase/Variation.js
@@ -4,12 +4,12 @@ import VariationGroup from "./VariationGroup";
 export default class Variation {
 
     /**
-     * 
-     * @param {number} id 
-     * @param {VariationGroup} variationGroup 
-     * @param {number} addedCost 
-     * @param {string} name 
-     * @param {DatabaseConnection} dbConn 
+     * Creates a new in-memory variation. This method is to be used by DatabaseConnection
+     * @param {number} id the database id of the variation
+     * @param {VariationGroup} variationGroup the group the variation belongs to
+     * @param {number} addedCost the added cost of the variation, in cents
+     * @param {string} name the name of the variation
+     * @param {DatabaseConnection} dbConn a connection to the BTS API
      */
     constructor(id, variationGroup, addedCost, name, dbConn) {
         this._id = id;
@@ -20,36 +20,68 @@ export default class Variation {
         this._blockers = [];
     }
 
+    /**
+     * 
+     * @returns the id of the variation
+     */
     getID() {
         return this._id;
     }
 
+    /**
+     * 
+     * @returns the group the variation blongs to
+     */
     getGroup() {
         return this._variationGroup;
     }
 
+    /**
+     * 
+     * @returns the cost, in cents, the variation adds to the product
+     */
     getAddedCost() {
         return this._addedCost;
     }
 
+    /**
+     * Updates the added cost the variation will add to the product whenever it is selected
+     * @param {number} newCost the added cost, in cents
+     */
     setAddedCost(newCost) {
         this._addedCost = newCost;
         this._variationGroup._product.markUnsaved();
     }
 
+    /**
+     * 
+     * @returns the name of the variation
+     */
     getName() {
         return this._name;
     }
 
+    /**
+     * Updates the name of the variation
+     * @param {string} newName the new name
+     */
     setName(newName) {
         this._name = newName;
         this._variationGroup._product.markUnsaved();
     }
 
+    /**
+     * Gets a list of blockers that prevent the selection of the variation
+     * @returns {Variation[]} the list of blockers
+     */
     getBlockers() {
         return this._blockers;
     }
 
+    /**
+     * Creates a new blocker between this variation, and the provided one
+     * @param {Variation} variation the variation to create a blocker with
+     */
     async addBlocker(variation) {
         if (!this._blockers.includes(variation)) {
             await this._dbConn.createNewBlocker(this, variation);
@@ -59,8 +91,8 @@ export default class Variation {
     }
 
     /**
-     * 
-     * @param {Variation} variation 
+     * Loads a blocker associated with the variation
+     * @param {Variation} variation the variation to load a blocker with
      */
     loadBlocker(variation) {
         if (!this._blockers.includes(variation)) {
@@ -69,8 +101,12 @@ export default class Variation {
         }
     }
 
+    /**
+     * Removes a blocker from the variation and database
+     * @param {Variation} variation 
+     */
     async deleteBlocker(variation) {
-
+        // TODO: Implement
     }
 
 }
