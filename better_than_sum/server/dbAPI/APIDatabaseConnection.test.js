22b2d9a180ff4880f5b14bf0becb92bf9c8bcbee
CodeAX2 <jacobhofer1@gmail.com>
2022-04-15 12:42:21 -0600

    Update CI

--- better_than_sum/server/dbAPI/APIDatabaseConnection.test.js
+++ better_than_sum/server/dbAPI/APIDatabaseConnection.test.js
@@ -11,3 +11,43 @@ test("Tests Database API", () => {
     });
 
 });
+
+test("Tests Database User Products", () => {
+    let dbConn = new DatabaseConnection("127.0.0.1", "root", "rootPassword", "G4db");
+    return dbConn.connect().then(() => {
+        return dbConn._getUserProductsJSON(1).then((results) => {
+            expect(JSON.parse(results).length).toBe(2);
+            return dbConn.disconnect();
+        });
+    });
+});
+
+test("Tests Database Products Variations", () => {
+    let dbConn = new DatabaseConnection("127.0.0.1", "root", "rootPassword", "G4db");
+    return dbConn.connect().then(() => {
+        return dbConn._getProductVariationsJSON(1).then((results) => {
+            expect(JSON.parse(results).length).toBe(5);
+            return dbConn.disconnect();
+        });
+    });
+});
+
+test("Tests Database Products Groups", () => {
+    let dbConn = new DatabaseConnection("127.0.0.1", "root", "rootPassword", "G4db");
+    return dbConn.connect().then(() => {
+        return dbConn._getVariationGroupsJSON(1).then((results) => {
+            expect(JSON.parse(results).length).toBe(2);
+            return dbConn.disconnect();
+        });
+    });
+});
+
+test("Tests Database Blockers", () => {
+    let dbConn = new DatabaseConnection("127.0.0.1", "root", "rootPassword", "G4db");
+    return dbConn.connect().then(() => {
+        return dbConn._getVariationBlockersJSON(2).then((results) => {
+            expect(JSON.parse(results).length).toBe(1);
+            return dbConn.disconnect();
+        });
+    });
+});
