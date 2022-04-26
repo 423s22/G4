332624514c1dd46b4a727678cb6e10105c511c57
CodeAX2 <jacobhofer1@gmail.com>
2022-04-26 10:29:06 -0600

    Add basic output for products

--- better_than_sum/AdminApp/ProductState.js
+++ better_than_sum/AdminApp/ProductState.js
@@ -11,78 +11,53 @@ export default class ProductState extends AppState {
 	onRender(divID) {
 		let div = document.getElementById(divID);
 
-		div.innerHTML = "";
-		div.innerHTML = "<h1>Products!</h1>";
-
-		// Temporary for testing db conn
-		let currentProducts = ["Phone", "Ipad", "Photo"];
-		for (let i = 0; i < currentProducts.length; i++) {
-			// Create an button which will allow for the drop down
-			let accordian = document.createElement("button");
-			let text = document.createTextNode(currentProducts[i]);
-			// Allow for calling in css
-			accordian.classList = "accordian";
-			accordian.id = currentProducts[i];
-			// Set button text
-			accordian.appendChild(text);
-			div.appendChild(accordian);
-
-			let panel = document.createElement("div");
-			panel.classList = "panel";
-			let paragraph = document.createElement("p");
-			var paratext = document.createTextNode(
-				"Test Text here for the feature release."
-			);
-			paragraph.appendChild(paratext);
-			panel.appendChild(paragraph);
-			div.appendChild(panel);
-
-			accordian.addEventListener("click", (event) => {
-				/* Toggle between adding and removing the "active" class,
-				to highlight the button that controls the panel */
-				accordian.classList.toggle("active");
-
-				/* Toggle between hiding and showing the active panel */
-				if (panel.style.display === "block") {
-					panel.style.display = "none";
-				} else {
-					panel.style.display = "block";
-				}
-			});
-		}
+		div.innerHTML = "Loading...";
 
 		this._app.getShopifyAPIConnection().getProductsJSON().then((shopifyProducts) => {
-
 			this._app.getDatabaseConnection().getUserProducts(this._app.getShopifyAPIConnection()).then((dbProducts) => {
+				div.innerHTML = "";
 				for (let i = 0; i < shopifyProducts["products"].length; i++) {
-					let curProduct = shopifyProducts["products"][i];
+					let curShopifyProduct = shopifyProducts["products"][i];
 
-					let shouldInclude = true;
+					let associatedBTSProduct = null;
 					for (let j = 0; j < dbProducts.getProducts().length; j++) {
-						if (dbProducts.getProducts()[j].getShopifyID() == curProduct["id"]) {
-							shouldInclude = false;
+						if (dbProducts.getProducts()[j].getShopifyID() == curShopifyProduct["id"]) {
+							associatedBTSProduct = dbProducts.getProducts()[j];
 							break;
 						}
 					}
 
-					if (!shouldInclude) continue;
+					if (associatedBTSProduct == null) {
 
-					let productDiv = document.createElement("div");
-					div.appendChild(productDiv);
+						let unusedProductDiv = document.createElement("div");
+						unusedProductDiv.classList.add("psUnusedProductDiv");
 
-					let productTitle = document.createElement("h3");
-					productTitle.textContent = curProduct["title"];
-					productDiv.appendChild(productTitle);
+						let productTitle = document.createElement("h1");
+						productTitle.innerText = curShopifyProduct["title"];
+						unusedProductDiv.appendChild(productTitle);
 
-					let addProductBtn = document.createElement("button");
-					addProductBtn.textContent = "Add Product";
-					addProductBtn.addEventListener("click", (e) => {
-						this._app.getDatabaseConnection().createNewProduct(curProduct).then((product) => {
-							console.log("Added Product!");
-							console.log(product);
-						});
-					});
-					productDiv.appendChild(addProductBtn);
+						console.log(curShopifyProduct);
+
+						div.appendChild(unusedProductDiv);
+
+					} else {
+						let btsProductDiv = document.createElement("div");
+						btsProductDiv.classList.add("psBTSProductDiv");
+
+						let productTitle = document.createElement("h1");
+						productTitle.innerText = associatedBTSProduct.getName();
+						btsProductDiv.appendChild(productTitle);
+
+						let productCost = document.createElement("h2");
+						productCost.innerText = "$" + (associatedBTSProduct.getBaseCost() / 100);
+						btsProductDiv.appendChild(productCost);
+
+						console.log(curShopifyProduct);
+
+						div.appendChild(btsProductDiv);
+					}
+
+
 				}
 			});
 
