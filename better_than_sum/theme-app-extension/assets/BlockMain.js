650696347b9b6c947c7ef2441b2d07771f7af1cb
CodeAX2 <jacobhofer1@gmail.com>
2022-05-02 14:54:09 -0600

    Add basic output for productID

--- better_than_sum/theme-app-extension/assets/BlockMain.js
+++ better_than_sum/theme-app-extension/assets/BlockMain.js
@@ -1,22 +1,21 @@
 function start() {
 
     let dbURL = document.getElementById("dbURL").value;
-    let productID = document.getElementById("shopifyProductID").value;
-    console.log(productID);
+    let shopifyProductID = document.getElementById("shopifyProductID").value;
 
     let url = new URL("https://" + dbURL);
     url.pathname += "database/";
-    url.searchParams.append("request", "userProducts");
-    url.searchParams.append("userID", "1");
+    url.searchParams.append("request", "shopifyProduct");
+    url.searchParams.append("shopifyID", shopifyProductID);
 
     let req = new XMLHttpRequest();
     req.open("GET", url.toString());
     req.onload = () => {
         let responseJSON = JSON.parse(req.responseText);
-        console.log(responseJSON);
+        let btsID = responseJSON["productID"];
+        console.log(btsID);
     }
     req.send();
-
 }
 
 start();
